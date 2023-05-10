afa33b86d7043f2a8568b8d544b168654d41ad2c
lohikaarme <stewart.watt@gmail.com>
2023-05-09 22:53:32 -0700

    Completed #my_each_with_index method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -3,15 +3,17 @@
 # require 'pry'
 
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    i = 0
+    for el in self do # rubocop:disable Style/For
+      yield el, i
+      i += 1
+    end
+    self
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
   def my_each
     for el in self do # rubocop:disable Style/For
       yield el
